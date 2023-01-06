e691094d79a62350621b29994668d6776f606192
BestChennn <bestchenbestchen@gmail.com>
2023-01-06 16:17:08 +0700

    Complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,25 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
+using namespace std;
+
+int main(){
+
+
+    string Grade[]={"A","B+","B","C+","C","D+","D","F","W"};
+    srand(time(0));
+    int rs = rand()%9;
+
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    
+    cout << "You will get "<<   Grade[rs]  <<" in this 261102." ;
+
+
+}
+
+
+
